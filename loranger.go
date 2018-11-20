3a95e4f4dde6425b4e95c3b96501291668650eee
crocdialer <crocdialer@gmail.com>
2018-11-20 11:56:26 +0100

    routing with gorilla mux, http part looking good

--- loranger.go
+++ loranger.go
@@ -7,72 +7,106 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/tarm/serial"
 )
 
 var device string
+var serialDevices []*serial.Port
+
+// create serial channels
+var serialInput, serialOutput chan []byte
+
+// nodes
+var nodes map[int]Node
 
 // Node structures information of a remote device
 type Node struct {
-	Address      uint8   `json:"src"`
+	Address      int     `json:"address"`
 	LastRssi     int     `json:"rssi"`
 	Mode         int     `json:"mode"`
 	Temperature  float64 `json:"temp"`
 	BatteryLevel float64 `json:"bat"`
 	TimeStamp    time.Time
-	GpsPosition  interface{} `json:"gps"`
+	GpsPosition  [2]float64 `json:"gps"`
 }
 
 // NodeCommand realizes a simple RPC interface
 type NodeCommand struct {
-	Address uint8         `json:"dst"`
+	Address int           `json:"dst"`
 	Command string        `json:"cmd"`
 	Params  []interface{} `json:"params"`
 }
 
-// NodeServer holds information of all remote devices that it had contact with
-type NodeServer struct {
-	nodes map[uint8]Node
-}
-
-// NewNodeServer creates an initialzed instance
-func NewNodeServer() *NodeServer {
-	newServer := &NodeServer{}
-	newServer.nodes = make(map[uint8]Node)
-	return newServer
-}
-
-// // parse something like this: {"src":1,"dst":0,"rssi":-25,"state":{"mode":0,"bat":1}}
-// func parseNode(input []byte) (err error, outNode Node) {
-// 	// var json_root interface{}
-// 	err = json.Unmarshal(input, &outNode)
-// 	outNode.TimeStamp = time.Now()
-// 	return err, outNode
-// }
-
-func (b *NodeServer) readData(input chan []byte) {
+func readData(input chan []byte) {
 	for line := range input {
 		var node Node
 		if err := json.Unmarshal(line, &node); err != nil {
 			log.Println("could not parse data as json:", string(line))
 		} else {
 			node.TimeStamp = time.Now()
-			b.nodes[node.Address] = node
-			outJSON, _ := json.Marshal(b.nodes)
-			log.Println(string(outJSON))
+			nodes[node.Address] = node
 		}
 	}
 }
 
-func (b *NodeServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// handle node requests:
+// /nodes
+// /nodes/{nodeID:[0-9]+}
+func handleNodes(w http.ResponseWriter, r *http.Request) {
+	log.Printf("request: %s\n", r.URL.Path[1:])
 	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
 	enc := json.NewEncoder(w)
-	enc.Encode(b.nodes)
-	// w.Write([]byte(fmt.Sprint(b.nodes)))
+
+	nodeID, ok := vars["nodeID"]
+
+	if ok {
+		k, _ := strconv.Atoi(nodeID)
+		log.Println("nodeID: ", k)
+		node, ok := nodes[k]
+
+		if ok {
+			enc.Encode(node)
+		}
+	} else {
+		// no nodeId provided, reply with a sorted list of all nodes
+		// To store the keys in slice in sorted order
+		var keys []int
+
+		for k := range nodes {
+			keys = append(keys, k)
+		}
+		sort.Ints(keys)
+		nodeList := make([]Node, len(nodes))
+
+		for i, k := range keys {
+			nodeList[i] = nodes[k]
+		}
+		enc.Encode(nodeList)
+	}
+}
+
+// POST
+func handleNodeCommand(w http.ResponseWriter, r *http.Request) {
+	// TODO: construct NodeCommand from POST-json
+	recCmd := &NodeCommand{1, "record", []interface{}{10}}
+	jsonStr, err := json.Marshal(recCmd)
+
+	if err != nil {
+		log.Println("could not marshal NodeCommand:", recCmd)
+	} else {
+		// send record command
+		log.Println("sending command:", string(jsonStr))
+		jsonStr = append(jsonStr, '\n')
+		serialOutput <- jsonStr
+	}
 }
 
 func readSerial(s *serial.Port, output chan<- []byte) {
@@ -101,6 +135,15 @@ func readSerial(s *serial.Port, output chan<- []byte) {
 	}
 }
 
+func writeData(input chan []byte) {
+	for bytes := range input {
+		for _, s := range serialDevices {
+			s.Write(bytes)
+			s.Flush()
+		}
+	}
+}
+
 func main() {
 	log.Println("welcome loranger")
 
@@ -109,10 +152,13 @@ func main() {
 	}
 
 	// init our NodeServer instance
-	nodeServer := NewNodeServer()
+	// nodeServer := NewNodeServer()
+
+	nodes = make(map[int]Node)
 
 	// create channel
-	serialInput := make(chan []byte, 100)
+	serialInput = make(chan []byte, 100)
+	serialOutput = make(chan []byte, 100)
 
 	files, err := ioutil.ReadDir("/dev")
 	if err != nil {
@@ -130,6 +176,7 @@ func main() {
 				continue
 			}
 			defer s.Close()
+			serialDevices = append(serialDevices, s)
 
 			log.Println("reading from", deviceName)
 
@@ -138,10 +185,27 @@ func main() {
 		}
 	}
 
-	// consumer
-	go nodeServer.readData(serialInput)
+	// consume incoming data
+	go readData(serialInput)
+
+	// deliver outgoing data to connected serials
+	go writeData(serialOutput)
+
+	// serve static files
+	fs := http.FileServer(http.Dir("static/"))
+
+	muxRouter := mux.NewRouter()
+
+	// services dealing with lora-nodes
+	muxRouter.HandleFunc("/nodes", handleNodes)
+	muxRouter.HandleFunc("/nodes/cmd", handleNodeCommand) //.Methods("POST")
+	muxRouter.HandleFunc("/nodes/{nodeID:[0-9]+}", handleNodes)
+	muxRouter.PathPrefix("/").Handler(fs)
+	http.Handle("/", muxRouter)
+
+	// http.Handle("/", fs)
 
 	port := 8080
 	log.Println("server listening on", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nodeServer))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
